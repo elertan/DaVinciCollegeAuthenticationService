8816784d4ee81538eeb9dadd93611b79a076a32b
elertan <elertan@ELERTAN-WIN10-V>
2016-09-29 12:07:35 +0200

    Added stuff

--- src/SignInAppTest/Models/ApplicationUser.cs
+++ src/SignInAppTest/Models/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace DaVinciCollegeAuthenticationService.Models
         public string UserNumber { get; set; }
         public bool IsTeacher { get; set; }
         public List<Application> Applications { get; set; }
+        // Does this work
     }
 }
