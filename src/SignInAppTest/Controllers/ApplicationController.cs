516ac404be7a14e3ad504ab4caf88b989a83404e
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2016-10-03 13:34:51 +0200

    Added things to the application controller and added application views

--- src/SignInAppTest/Controllers/ApplicationController.cs
+++ src/SignInAppTest/Controllers/ApplicationController.cs
@@ -4,20 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DaVinciCollegeAuthenticationService.Services;
+using DaVinciCollegeAuthenticationService.Data;
+using DaVinciCollegeAuthenticationService.Models.ApplicationViewModels;
+using DaVinciCollegeAuthenticationService.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DaVinciCollegeAuthenticationService.Controllers
 {
+    [Authorize]
     public class ApplicationController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ITokenProvider _tokenProvider;
-        public ApplicationController(ITokenProvider tokenProvider)
+        public ApplicationController(UserManager<ApplicationUser> userManager, ApplicationDbContext context, ITokenProvider tokenProvider)
         {
+            _context = context;
+            _userManager = userManager;
             _tokenProvider = tokenProvider;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            return View(new IndexViewModel() { Applications = user.Applications });
+        }
+
+
+        public IActionResult Create()
+        {
+            return View(new CreateViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var application = new Application() { Name = model.Name, LoginCallbackUrl = model.LoginCallbackUrl };
+                _context.Applications.Add(application);
+                var contextUser = _context.ApplicationUser.First(u => u.Id == user.Id);
+                contextUser.Applications.Add(application);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(ApplicationController.Create));
+            }
+
+            return View(model);
         }
     }
 }
