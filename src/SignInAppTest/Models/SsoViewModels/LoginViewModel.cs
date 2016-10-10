bf41fe86440bfe0ba0960563088512e91da1db3d
elertan <elertan@ELERTAN-WIN10-V>
2016-10-10 12:08:40 +0200

    Added appsession

---
+++ src/SignInAppTest/Models/SsoViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaVinciCollegeAuthenticationService.Models.SsoViewModels
+{
+    public class LoginViewModel
+    {
+        public Application Application { get; set; }
+        public string ApplicationSession { get; set; }
+    }
+}
