98c5b16ef224bdd62ddac8974d5cb0a2484e4103
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2016-11-14 10:39:24 +0100

    change password

--- src/SignInAppTest/Services/MessageServices.cs
+++ src/SignInAppTest/Services/MessageServices.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Threading.Tasks;
 
 namespace DaVinciCollegeAuthenticationService.Services
 {
@@ -7,11 +9,38 @@ namespace DaVinciCollegeAuthenticationService.Services
     // For more details see this link http://go.microsoft.com/fwlink/?LinkID=532713
     public class AuthMessageSender : IEmailSender, ISmsSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public Task SendEmailAsync(string emailAddressTo, string subject, string message)
         {
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(new MailboxAddress("Sebas Bakker", "sebasbakker8@hotmail.com"));
+            mailMessage.To.Add(new MailboxAddress("Patrick Vonk", emailAddressTo));//the name needs to be changed to something else
+            mailMessage.Subject = subject;
+            mailMessage.Body = new TextPart("plain")
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("Localhost", 9001, false);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                //// Note: only needed if the SMTP server requires authentication
+                //client.Authenticate("joey", "password");
+
+                client.Send(mailMessage);
+                client.Disconnect(true);
+            }
+
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
-        }
+        }//Handel hier FakeSMTP Af
 
         public Task SendSmsAsync(string number, string message)
         {
