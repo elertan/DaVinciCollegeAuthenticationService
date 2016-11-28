e61fe32976f831edfeeec1586a9e5a17d181a923
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2016-11-28 09:30:06 +0100

    Added admin Panel

--- src/SignInAppTest/Controllers/ExcelController.cs
+++ src/SignInAppTest/Controllers/ExcelController.cs
@@ -74,12 +74,7 @@ namespace DaVinciCollegeAuthenticationService.Controllers
                 var result = await _userManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
-                    // Send an email with this link
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-                    await _emailSender.SendEmailAsync(user.Email, "Confirm your account", $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-                    await _signInManager.SignInAsync(user, false);
+
                 }
             }
 
