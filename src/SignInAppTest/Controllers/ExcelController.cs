4322437433a5e52632d275f1145f9e23d5586c90
elertan <elertan@ELERTAN-WIN10-V>
2016-11-28 09:28:50 +0100

    Added extra check to prevent database corruption

--- src/SignInAppTest/Controllers/ExcelController.cs
+++ src/SignInAppTest/Controllers/ExcelController.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using System.IO;
-using DaVinciCollegeAuthenticationService.Models;
-using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using DaVinciCollegeAuthenticationService.Data;
+using DaVinciCollegeAuthenticationService.Models;
 using DaVinciCollegeAuthenticationService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,70 +17,65 @@ namespace DaVinciCollegeAuthenticationService.Controllers
     public class ExcelController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailSender _emailSender;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ExcelController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IEmailSender emailSender)
+        public ExcelController(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, IEmailSender emailSender)
         {
             _context = context;
             _userManager = userManager;
             _emailSender = emailSender;
             _signInManager = signInManager;
         }
+
         public IActionResult Index()
         {
-            ViewBag.text = "mark";
             return View();
         }
+
 
         [HttpPost]
         public async Task<IActionResult> PostExcelSheet(IFormFile file)
         {
             byte[] fileBytes = null;
-            BinaryReader reader = new BinaryReader(file.OpenReadStream());
-            fileBytes = reader.ReadBytes((int)file.Length);
+            var reader = new BinaryReader(file.OpenReadStream());
+            fileBytes = reader.ReadBytes((int) file.Length);
 
-            string fileDataResult = System.Text.Encoding.UTF8.GetString(fileBytes);
+            var fileDataResult = Encoding.UTF8.GetString(fileBytes);
             ViewBag.text = fileDataResult;
-            List<string> rows = new List<string>();
-            foreach (var item in fileDataResult.Split(new char[] { '"' }))
+            var rows = new List<string>();
+            foreach (var item in fileDataResult.Split('"'))
             {
-                if (item.Length > 1)
-                {
-                    int testResult;
-                    if (int.TryParse(item[1].ToString(), out testResult))
-                    {
-                        rows.Add(item);
-                    }
-                }
+                if (item.Length <= 1) continue;
+
+                int testResult;
+                if (int.TryParse(item[1].ToString(), out testResult))
+                    rows.Add(item);
             }
 
-            foreach (var row in rows)
-            {
-                var splittedRow = row.Split(new char[] { ';' });//TODO: add 3 values to ApplicationUser the FirstName, MiddleName and LastName
-                var userName = splittedRow[0];
-                var firstName = splittedRow[1];
-                var middleName = splittedRow[2];
-                var lastName = splittedRow[3];
-
-                var user = new ApplicationUser
+            var newUsers = (from row in rows
+                select row.Split(';')
+                into splittedRow
+                let userName = splittedRow[0]
+                let firstName = splittedRow[1]
+                let middleName = splittedRow[2]
+                let lastName = splittedRow[3]
+                select new ApplicationUser
                 {
                     UserName = userName,
-                    Email = userName + "@mydavinci.nl"//Only works for students with an email like this 99027705@mydavinci.nl
-                };
+                    Email = userName + "@mydavinci.nl"
+                    //Only works for students with an email like this 99027705@mydavinci.nl
+                }).ToList();
 
-                //var mark = await _userManager.DeleteAsync(user);
+            foreach (var user in newUsers)
+            {
                 var result = await _userManager.CreateAsync(user);
-                if (result.Succeeded)
-                {
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
-                    // Send an email with this link
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-                    await _emailSender.SendEmailAsync(user.Email, "Confirm your account", $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-                    await _signInManager.SignInAsync(user, false);
-                }
+                if (!result.Succeeded) continue;
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(error.Code, error.Description);
+                break;
             }
 
             return View(nameof(Index));
